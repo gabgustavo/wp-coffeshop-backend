c0e3d2fa64466258c7b0586e09933fe9960c579b
gabgustavo <gustavoavilabar@hotmail.com>
2026-06-20 15:34:54 -0500

    pagina de galeria, custom campos para rest api y custom query para categorias e imagenes

--- app/public/wp-content/themes/AppRestApi/functions.php
+++ app/public/wp-content/themes/AppRestApi/functions.php
@@ -11,12 +11,20 @@ function app_restapi_setup() {
 add_action('after_setup_theme', 'app_restapi_setup');
 
 function app_restapi_api_init() {
-    register_rest_field(['page'], 
+    register_rest_field(['page', 'post'], 
         'featured_image',
         [
             'get_callback' => 'get_featured_image'
         ]
     );
+
+    register_rest_field(['post'], 'category_list', [
+        'get_callback' => 'get_post_categories'
+    ]);
+
+    register_rest_field(['page'], 'gallery', [
+        'get_callback' => 'get_gallery'
+    ]);
 }
 add_action('rest_api_init', 'app_restapi_api_init');
 
@@ -41,4 +49,44 @@ function get_featured_image($post) {
     
 
     return $images;
+}
+
+function get_post_categories($post) {
+
+    return array_map(function($category_id){
+        $category = get_category($category_id, 'ARRAY_A');
+        return [
+            'name'=> $category['name'],
+            'slug'=> $category['slug'],
+            'id'=> $category['cat_ID'],
+        ];
+    }, $post['categories']);
+}
+
+function get_gallery($post) {
+    if($post['slug'] !== 'galeria') {
+        return [];
+    }
+    $gallery = get_post_gallery($post['id'], false);
+    $gallery_ids = array_map('intval', explode(',', $gallery['ids']));
+
+
+    return array_map(
+        function ($image_id) {
+            $large_image = wp_get_attachment_image_src($image_id, 'large');
+            $full_image = wp_get_attachment_image_src($image_id, 'full');
+            return [
+                'large' => [
+                    'url' => $large_image[0],
+                    'width' => $large_image[1],
+                    'height' => $large_image[2],
+                ],
+                'full' => [
+                    'url' => $full_image[0],
+                    'width' => $full_image[1],
+                    'height' => $full_image[2],
+                ],
+            ];
+        }, $gallery_ids
+    );
 }
